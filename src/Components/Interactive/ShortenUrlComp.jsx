f18ed6fc2b0853e959b0b54109740d47e3ffebf0
Nonso <nonsootoh@gmail.com>
2021-07-12 16:38:04 -0400

    functionality

--- src/Components/Interactive/ShortenUrlComp.jsx
+++ src/Components/Interactive/ShortenUrlComp.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import ShortLinks from "./ShortLinks";
 
 const ShortenUrlComp = () => {
+  const [userUrl, setUserUrl] = useState("");
+  const [resp, setResp] = useState();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (userUrl === "") {
+      alert("please enter in a URL");
+      return;
+    }
+
+    axios.get(`https://api.shrtco.de/v2/shorten?url=${userUrl}`).then((res) => {
+      const responseProf = res.data;
+      setResp(responseProf);
+    });
+  };
+  console.log(resp);
   return (
     <section className="ShortenCompCon">
-      <form className="ShortenForm">
+      <form className="ShortenForm" onSubmit={onSubmit}>
         <input
           type="text"
           className="ShortenForm__Input"
           placeholder="Shorten a link here..."
+          defaultValue={userUrl}
+          onChange={(e) => setUserUrl(e.target.value)}
         />
-        <input type="button" className="ShortenForm__btn" value="Shorten it!" />
+        <input type="submit" className="ShortenForm__btn" value="Shorten it!" />
       </form>
       <div className="ShortLinksCon">
         <ShortLinks />
